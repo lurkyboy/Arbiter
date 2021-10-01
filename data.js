a7b54e41743ac985b960925495cafe6d626922e1
lurkyboy <90408004+lurkyboy@users.noreply.github.com>
2021-10-01 14:52:52 -0400

    Add files via upload
    testing 10/1/21

---
+++ data.js
@@ -0,0 +1,518 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nadir-test-1",
+      "name": "NADIR TEST 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-nadir-test-2",
+      "name": "NADIR TEST 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-front-1",
+      "name": "FRONT 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5229180320847107
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20589786292050505,
+          "pitch": 0.3083813607582435,
+          "rotation": 0,
+          "target": "3-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6951352107444695,
+          "pitch": -0.35786622707280813,
+          "title": "Scanner",
+          "text": "[would it be better to have info spots right above a thing, or like on the thing"
+        },
+        {
+          "yaw": -0.18272003626583455,
+          "pitch": -0.913398467929273,
+          "title": "Digital Design Studio at Michael Schwartz Library",
+          "text": "Information about the DDS"
+        }
+      ]
+    },
+    {
+      "id": "3-entrance",
+      "name": "ENTRANCE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.061096251192111595,
+          "pitch": 0.3848028268685546,
+          "rotation": 0,
+          "target": "8-computersback-too"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9157779999316276,
+          "pitch": 0.02322635174432719,
+          "title": "Front Desk",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.8705105603691692,
+          "pitch": -0.049197511901446944,
+          "title": "Computers Here Too",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4603785710570385,
+          "pitch": -0.2323666975597387,
+          "title": "The case of fancy things",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-img_20211001_122724",
+      "name": "IMG_20211001_122724",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-img_20211001_122742",
+      "name": "IMG_20211001_122742",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-img_20211001_123059",
+      "name": "IMG_20211001_123059",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-front-desk-1",
+      "name": "FRONT DESK 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-computersback-too",
+      "name": "COMPUTERS/BACK TOO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.630400697551508,
+          "pitch": 0.29534254705031415,
+          "rotation": 0,
+          "target": "12-back-2-better"
+        },
+        {
+          "yaw": -1.3790408067968691,
+          "pitch": 0.26628916648681233,
+          "rotation": 0,
+          "target": "3-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20191013754653575,
+          "pitch": -0.18679352002100202,
+          "title": "the Mac computers",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.5223740880798458,
+          "pitch": -0.09299313081624483,
+          "title": "the wall of narrow doors",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-img_20211001_123407",
+      "name": "IMG_20211001_123407",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-img_20211001_123414",
+      "name": "IMG_20211001_123414",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-back-2",
+      "name": "BACK 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-back-2-better",
+      "name": "BACK 2 BETTER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 1.236277348149402,
+        "pitch": 0.2678549221128428,
+        "fov": 1.5229180320847107
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5207194597246954,
+          "pitch": 0.08104663229483577,
+          "rotation": 0,
+          "target": "3-entrance"
+        },
+        {
+          "yaw": 1.5962089238717656,
+          "pitch": 0.08840045029928412,
+          "rotation": 0,
+          "target": "8-computersback-too"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7840800129456706,
+          "pitch": 0.1268628125073974,
+          "title": "Picture thing",
+          "text": "Useful for product photography, etc."
+        },
+        {
+          "yaw": 0.1419288677682431,
+          "pitch": 0.32255073026851555,
+          "title": "Would be good to take a picture over here",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9929783711026872,
+          "pitch": 0.045809352532474534,
+          "title": "Theres stuff over here that should be linked to as well",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "DDR Tour Test 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
